8a665ca3aa68b30ebad6bab7f739205226d4c92e
Paulo Victorio <paulo_victorio@yahoo.com>
2022-12-22 01:18:10 +0800

    Commit minor changes

--- script/script.js
+++ script/script.js
@@ -1,23 +1,94 @@
+const operationScreen = document.querySelector('.operation-screen');
+const resultScreen = document.querySelector('.result-screen')
 const keys = [...document.querySelectorAll('.button')];
+let operationArray = [];
+let resultArray = [];
+let operand1 = [];
+let operand2 = [];
+let operator = '';
 
-// 
+// Indicates whether operand2 array is to be accessed (isOperand2Accessed = true)
+let isOperand2Accessed = false;
+
+// Handle displaying of operation
 function displayOperation() {
+  // operationScreen.textContent = operand1.join('')
+  //                                        // Adds commas as thousands separator
+  //                                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",") 
+  //                               // + ' '
+  //                               + operator
+  //                               // + ' '
+  //                               + operand2.join('')
+  //                                        // Adds commas as thousands separator
+  //                                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",") ;
+  // operationArray = [...operationScreen.textContent];
+  // console.log(operationArray);
+}
 
+// Handle displaying of result
+function displayInput() {
+  // resultScreen.textContent = 
 }
 
 //
-function displayResult() {
-
+function allClear() {
+  operationScreen.textContent = '';
+  resultScreen.textContent = '';
+  operand1 = [];
+  operand2 = [];
+  operator = '';
 }
 
-// Handle keyboard input
-window.addEventListener('keydown', e => {
-  const key = document.querySelector(`.button[data-key="${e.key}"]`);
-  if (keys.includes(key)) console.log(key);
-});
+//
+function clearEntry() {
+  if (!isOperand2Accessed) operand1.pop();
+  else operand2.pop();
 
-// Handle onscreen input
-window.addEventListener('click', e => {
-  const key = e.target;
-  if (keys.includes(key)) console.log(key);
-});
+  displayOperation();
+}
+
+//
+function operate(a, b, operator) {
+  if (operator === '+') return a + b;
+
+  if (operator === '-') return a - b;
+
+  if (operator === '*') return a * b;
+
+  if (operator === '/') return a / b;
+}
+
+const events = ['keydown', 'click'];
+
+events.forEach(event => window.addEventListener(event, e => {
+
+  // Handle both keyboard and onscreen input
+  const key = document.querySelector(`.button[data-key="${e.key}"]`) || e.target;
+  
+  if (keys.includes(key)) {
+
+    // All clear
+    if (key.getAttribute('id') === 'ac') allClear();
+
+    // Clear last entry
+    if (key.getAttribute('id') === 'ce') clearEntry();
+
+    // Number
+    if (key.getAttribute('class').split(' ').includes('nums')) {
+      // if (!isOperand2Accessed) operand1.push(key.textContent);
+      // else operand2.push(key.textContent);
+
+      resultArray.push(key.textContent);
+      displayInput();
+    }
+
+    // Operation key
+    if (key.getAttribute('class').split(' ').includes('operation')) {
+      isOperand2Accessed = true;
+      operator = key.textContent;
+      displayOperation();
+    }
+
+
+  }
+}));
--- script/toggle-mode.js
+++ script/toggle-mode.js
@@ -2,9 +2,6 @@ const light = document.querySelector('#light-mode');
 const dark = document.querySelector('#dark-mode');
 const checkbox = document.querySelector('.checkbox')
 
-function btnChrome_onclick() {
-  document.documentElement.webkitRequestFullScreen();
-}
 
 checkbox.addEventListener('click', e => {
   if (!e.target.checked) {
@@ -15,6 +12,4 @@ checkbox.addEventListener('click', e => {
     light.disabled = 'disabled';
     dark.disabled = undefined;
   }
-
-  btnChrome_onclick();
 });
